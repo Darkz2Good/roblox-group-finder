fc42c47844681d52289b1678fca16937ba10f6e2
Darkz2Good <151495135+Darkz2Good@users.noreply.github.com>
2023-11-20 04:54:05 +0000

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ async function findOpenGroups() {
     try {
       const group = await axios.get(`https://groups.roproxy.com/v1/groups/${i}`);
       if (group.data.publicEntryAllowed === true && group.data.owner === null) {
-        await axios.post('YOUR_WEBHOOK_URL', {
+        await axios.post('https://discord.com/api/webhooks/1176021355053994117/R1Wd9XlwMRbT1brG1C2Jx6P8oOx7DR3Qf54dAKtIK-Qhl8iYU8cWBdvNg7PHdOJF-j-U', {
           content: `Found an open group: https://www.roblox.com/groups/${i}`
         });
       }
